345a36fd0afc6334ae48fb265a919f7185dae7cf
Manuel Annabring <manuel.annabring@icloud.com>
2026-03-27 13:45:15 +0100

    Initial Commit

---
+++ Wheelie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wheelie
+//
+//  Created by Manuel on 27.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wheelie/WheelieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelieApp.swift
+//  Wheelie
+//
+//  Created by Manuel on 27.03.26.
+//
+
+import SwiftUI
+
+@main
+struct WheelieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
